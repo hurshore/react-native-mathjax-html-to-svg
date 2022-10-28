6e90c692357f56945b122d129a3f47330b5b99bb
Jahtofunmi Osho <oshotofunmi@gmail.com>
2022-10-28 11:51:51 +0100

    fix children type issue

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,13 @@
 declare module 'react-native-mathjax-html-to-svg' {
-    import {StyleProp, ViewStyle} from 'react-native';
+  import { StyleProp, ViewStyle } from 'react-native';
 
-    type Props = {
-        fontSize?: number
-        color?: string
-        fontCache?: boolean
-        style?: StyleProp<ViewStyle>
-    };
+  type Props = {
+    children: React.ReactNode;
+    fontSize?: number;
+    color?: string;
+    fontCache?: boolean;
+    style?: StyleProp<ViewStyle>;
+  };
 
-    export function MathJaxSvg(props: Props) ;
+  export function MathJaxSvg(props: Props);
 }
