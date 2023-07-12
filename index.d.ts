1910a9891018df3549045396ab546b6258e0eaf8
Jahtofunmi Osho <oshotofunmi@gmail.com>
2023-07-12 17:01:16 +0100

    add support for text props

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,16 @@
-declare module 'react-native-mathjax-html-to-svg' {
-    import {TextStyle, ViewStyle} from 'react-native';
-    import {ReactNode} from 'react';
+declare module '@hurshore/react-native-mathjax-html-to-svg' {
+  import { TextProps, TextStyle, ViewStyle } from 'react-native';
+  import { ReactNode } from 'react';
 
-    type Props = {
-        fontSize?: number,
-        color?: string,
-        fontCache?: boolean,
-        style?: ViewStyle,
-        textStyle?: TextStyle,
-        children?: ReactNode,
-    };
+  type Props = {
+    fontSize?: number;
+    color?: string;
+    fontCache?: boolean;
+    style?: ViewStyle;
+    textStyle?: TextStyle;
+    textProps?: TextProps;
+    children?: ReactNode;
+  };
 
   export function MathJaxSvg(props: Props);
 }
